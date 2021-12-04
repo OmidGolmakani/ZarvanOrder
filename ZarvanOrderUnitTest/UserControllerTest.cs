7b8f6ac475c9f4a0cb46a7139cf60fcf1a020ead
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-04 23:20:43 +0330

    Create UnitTesting

---
+++ ZarvanOrderUnitTest/UserControllerTest.cs
@@ -0,0 +1,32 @@
+using Moq;
+using System;
+using Xunit;
+using ZarvanOrder.Controllers;
+using ZarvanOrder.Interfaces.DataServices;
+using ZarvanOrder.Model.Dtos.Requests.Users;
+using ZarvanOrder.Model.Dtos.Responses.Users;
+
+namespace ZarvanOrderUnitTest
+{
+    public class UserControllerTest
+    {
+        public UserControllerTest()
+        {
+        }
+        [Fact]
+        public void GetsAsync_WithUnexistingItem_ReturnNotFound()
+        {
+            ///Arrage
+            var userServiceSetup = new Mock<IUserService>();
+            userServiceSetup.Setup(srv => srv.GetAsync(It.IsAny<GetUserRequest>())).
+                ReturnsAsync((UserResponse)null);
+            var controller = new UserController(userServiceSetup.Object);
+            ///Act
+            var result = await controller.Gets(new GetUsersRequest() {})
+
+            ///Assert
+
+        }
+
+    }
+}
