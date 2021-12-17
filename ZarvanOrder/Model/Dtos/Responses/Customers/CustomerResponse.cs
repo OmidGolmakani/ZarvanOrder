a9839dd1eb3e68852d43fdb4e13439453b7ff27f
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-17 13:22:09 +0330

    Add Customer Response

---
+++ ZarvanOrder/Model/Dtos/Responses/Customers/CustomerResponse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarvanOrder.Model.Dtos.Responses.Customers
+{
+    public class CustomerResponse : Bases.BaseResponse
+    {
+        public long Id { get; set; }
+        public long RefId { get; set; }
+        public long UserId { get; set; }
+        public string UserFullName { get; set; }
+        public byte CustomerTypeId { get; set; }
+        public string CustomerType { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Family { get; set; }
+        public string CompanyName { get; set; }
+        public decimal Reminded { get; set; }
+    }
+}
