0d10ee244848dab9db0e12ea5f7fa41936f05f4e
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-18 00:50:36 +0330

    Update Customer Profile

--- ZarvanOrder/Configs/AutoMapper/CustomerProfile.cs
+++ ZarvanOrder/Configs/AutoMapper/CustomerProfile.cs
@@ -9,7 +9,7 @@ namespace ZarvanOrder.Configs.AutoMapper
             CreateMap<Model.Entites.Customer, Model.Dtos.Responses.Customers.CustomerResponse>()
             .ForMember(dest => dest.CustomerTypeId, opt => opt.MapFrom(src => src.CustomerType))
             .ForMember(dest => dest.UserFullName, opt => opt.MapFrom(src => $"{src.UserCustomer.Name} {src.UserCustomer.Family}"))
-            .ForMember(dest => dest.CustomerType, opt => opt.MapFrom(src => Helpers.Enumable.Find(typeof(Model.Enums.Customer),src.CustomerType)));
+            .ForMember(dest => dest.CustomerType, opt => opt.MapFrom(src => Helpers.Enumerated.Find(typeof(Model.Enums.Customer),src.CustomerType)));
             CreateMap<Model.Dtos.Responses.Customers.CustomerResponse, Model.Entites.Customer>();
             CreateMap<Model.Entites.Customer, Model.Dtos.Requests.Customers.AddCustomerRequest>().ReverseMap();
             CreateMap<Model.Entites.Customer, Model.Dtos.Requests.Customers.EditCustomerRequest>().ReverseMap();
