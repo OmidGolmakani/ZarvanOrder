0f08e66f2a03a2cfa8af87ef0b5c161def1283e0
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-18 15:29:58 +0330

    .

--- ZarvanOrder/Repositores/CustomerRepository.cs
+++ ZarvanOrder/Repositores/CustomerRepository.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ZarvanOrder.Interfaces.Repositores;
+using ZarvanOrder.Model.Dtos.Requests.Customers;
+using ZarvanOrder.Model.Entites;
 
 namespace ZarvanOrder.Repositores
 {
-    public class CustomerRepository : ICustomerRepository
+    public class CustomerRepository : ZarvanOrder.Repositores.Repository<long, Model.Entites.Customer>, ICustomerRepository
     {
+        public IQueryable<User> Get(GetCustomerRequest request, bool includeDeleted = false)
+        {
+            throw new NotImplementedException();
+        }
 
+        public async Task<Customer> GetById(GetCustomerRequest request, bool includeDeleted = false)
+        {
+            this.DbSet.FirstOrDefaultAsync(p=> p.)
+        }
     }
 }
