566ff795654c6d12abad29ccdd874bf1e39a2f4f
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-20 16:07:44 +0330

    create Iregion repo

---
+++ ZarvanOrder/Interfaces/Repositores/IRegionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZarvanOrder.Model.Dtos.Requests.Regions;
+using ZarvanOrder.Model.Entites;
+
+namespace ZarvanOrder.Interfaces.Repositores
+{
+    public interface IRegionRepository : IRepository<int, Model.Entites.Region>, IGetRepository<GetRegionRequest, GetsRegionRequest, Region>
+    {
+
+    }
+}
