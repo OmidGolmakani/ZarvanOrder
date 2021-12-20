b1b48c6bc84d0ab935a45d00859896ea8df02a5b
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-20 15:49:45 +0330

    Update ICustomerRepository And Inherit IGetRepository

--- ZarvanOrder/Interfaces/Repositores/ICustomerRepository.cs
+++ ZarvanOrder/Interfaces/Repositores/ICustomerRepository.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ZarvanOrder.Interfaces.Repositores;
-using ZarvanOrder.Model.Dtos.Requests.Customers;
+﻿using ZarvanOrder.Model.Dtos.Requests.Customers;
+using ZarvanOrder.Model.Entites;
 
 namespace ZarvanOrder.Interfaces.Repositores
 {
-    public interface ICustomerRepository : IRepository<long, Model.Entites.Customer>
+    public interface ICustomerRepository : IRepository<long, Model.Entites.Customer>, 
+                                           IGetRepository<GetCustomerRequest, GetCustomersRequest, Customer>
     {
-        Task<Model.Entites.Customer> GetById(GetCustomerRequest request, bool includeDeleted = false);
-        IQueryable<Model.Entites.Customer> Get(GetCustomersRequest request, bool includeDeleted = false);
+
     }
 }
