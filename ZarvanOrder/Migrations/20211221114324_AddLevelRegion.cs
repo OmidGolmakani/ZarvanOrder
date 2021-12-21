4758b1b8a340c8ff9f8c93c8054a5b497da516b0
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-21 15:15:19 +0330

    add level to region

---
+++ ZarvanOrder/Migrations/20211221114324_AddLevelRegion.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ZarvanOrder.Migrations
+{
+    public partial class AddLevelRegion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customer_AspNetUsers_UserId",
+                table: "Customer");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customer",
+                table: "Customer");
+
+            migrationBuilder.RenameTable(
+                name: "Customer",
+                newName: "Customers");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Customer_UserId",
+                table: "Customers",
+                newName: "IX_Customers_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Region",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RegionFatherId = table.Column<int>(type: "int", nullable: true),
+                    RefId = table.Column<int>(type: "int", nullable: false),
+                    RegionCode = table.Column<int>(type: "int", nullable: false),
+                    RegionName = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Level = table.Column<byte>(type: "tinyint", nullable: false, defaultValueSql: "1"),
+                    Active = table.Column<bool>(type: "bit", nullable: false, defaultValueSql: "1"),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DeletedBy = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: true),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Region", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Region_Region_RegionFatherId",
+                        column: x => x.RegionFatherId,
+                        principalTable: "Region",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Region_RegionFatherId",
+                table: "Region",
+                column: "RegionFatherId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_AspNetUsers_UserId",
+                table: "Customers",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_AspNetUsers_UserId",
+                table: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "Region");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Customers",
+                table: "Customers");
+
+            migrationBuilder.RenameTable(
+                name: "Customers",
+                newName: "Customer");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Customers_UserId",
+                table: "Customer",
+                newName: "IX_Customer_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Customer",
+                table: "Customer",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customer_AspNetUsers_UserId",
+                table: "Customer",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
