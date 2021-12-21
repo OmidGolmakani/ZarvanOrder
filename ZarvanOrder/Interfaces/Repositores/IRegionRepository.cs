2d9343a81c04ed5d44beac2e62255816e2923e3c
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-21 12:52:22 +0330

    .

--- ZarvanOrder/Interfaces/Repositores/IRegionRepository.cs
+++ ZarvanOrder/Interfaces/Repositores/IRegionRepository.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ZarvanOrder.Model.Dtos.Requests.Regions;
+﻿using ZarvanOrder.Model.Dtos.Requests.Regions;
 using ZarvanOrder.Model.Entites;
 
 namespace ZarvanOrder.Interfaces.Repositores
 {
-    public interface IRegionRepository : IRepository<int, Model.Entites.Region>, IGetRepository<GetRegionRequest, GetsRegionRequest, Region>
+    public interface IRegionRepository : IRepository<int, Region>, IGetRepository<GetRegionRequest, GetsRegionRequest, Region>
     {
 
     }
