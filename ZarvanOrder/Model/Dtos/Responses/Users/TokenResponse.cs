1ba82afac825e31fc665d1cf38a20071c9bffee0
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-21 14:36:26 +0330

    update user respose after add generic to base class

--- ZarvanOrder/Model/Dtos/Responses/Users/TokenResponse.cs
+++ ZarvanOrder/Model/Dtos/Responses/Users/TokenResponse.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ZarvanOrder.Model.Dtos.Responses.Users
 {
-    public class TokenResponse : Bases.BaseResponse
+    public class TokenResponse : Bases.BaseResponse<long>
     {
+        [JsonIgnore]
+        public override long Id { get => base.Id; set => base.Id = value; }
         public string Value { get; set; }
     }
 }
