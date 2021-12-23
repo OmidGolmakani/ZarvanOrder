3a45543102a8849fb5a2b0f60c43937f0fc59789
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-24 01:34:42 +0330

    Change a Bad NameSpace

--- ZarvanOrder/Model/Dtos/Requests/MarketerAccessRegions/AddMarketerAccessRegionRequest.cs
+++ ZarvanOrder/Model/Dtos/Requests/MarketerAccessRegions/AddMarketerAccessRegionRequest.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ZarvanOrder.Model.Dtos.Requests.Customers
+namespace ZarvanOrder.Model.Dtos.Requests.MarketerAccessRegions
 {
     public class AddMarketerAccessRegionRequest : Bases.AddRequest
     {
