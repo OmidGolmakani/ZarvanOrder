c2314447e0bef5606a403c3edf6147dbc8a1c92d
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-25 00:29:45 +0330

    Create GoodsGroup Entity

---
+++ ZarvanOrder/Model/Entites/GoodsGroup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarvanOrder.Model.Entites
+{
+    public class GoodsGroup : AuditDeleteEntity<int>
+    {
+        public int FatherId { get; set; }
+        public int RefId { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public GoodsGroup FatherGoodsGroup { get; set; }
+        public ICollection<GoodsGroup> ChildGoodsGroups { get; set; }
+    }
+}
