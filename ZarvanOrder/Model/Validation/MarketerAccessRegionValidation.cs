e95faca0e929c40adbf4f9efae1639a556dc98d7
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-24 12:48:33 +0330

    create marketer access region validation

--- ZarvanOrder/Model/Validation/MarketerAccessRegionValidation.cs
+++ ZarvanOrder/Model/Validation/MarketerAccessRegionValidation.cs
@@ -4,13 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZarvanOrder.Interfaces.DataServices;
 
 namespace ZarvanOrder.Model.Validation
 {
     public class MarketerAccessRegionValidation : AbstractValidator<Model.Entites.MarketerAccessRegion>
     {
-        public MarketerAccessRegionValidation()
+        private readonly IMarketerAccessRegionService service;
+
+        public MarketerAccessRegionValidation(IMarketerAccessRegionService service)
         {
+            this.service = service;
             this.RuleFor(p => p.UserId).NotNull().WithMessage("بازاریاب اجباری می باشد");
             this.RuleFor(p => p.RegionId).NotNull().WithMessage("منطقه مشخص نشده است");
         }   
