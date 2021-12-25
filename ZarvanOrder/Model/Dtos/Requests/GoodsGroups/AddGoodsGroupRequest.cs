fb841276a893c8e0bb2fa34207dca5d937c455c8
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-25 12:46:35 +0330

    create goods group request

---
+++ ZarvanOrder/Model/Dtos/Requests/GoodsGroups/AddGoodsGroupRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarvanOrder.Model.Dtos.Requests.GoodsGroups
+{
+    public class AddGoodsGroupRequest : Bases.AddRequest
+    {
+        public int FatherId { get; set; }
+        public int RefId { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ZarvanOrder/Model/Dtos/Requests/GoodsGroups/DeleteGoodsGroupRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarvanOrder.Model.Dtos.Requests.GoodsGroups
+{
+    public class DeleteGoodsGroupRequest : Bases.DeleteRequest<int>
+    {
+
+    }
+}
