dd58359ff683a548db6933070c89d4e9bb4556d5
Omid Golmakani <OmidGolmakani@outlook.com>
2021-12-25 12:46:55 +0330

    create goods group response

---
+++ ZarvanOrder/Model/Dtos/Responses/GoodsGroups/GoodsGroupResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZarvanOrder.Model.Dtos.Responses.GoodsGroups
+{
+    public class GoodsGroupResponse : Bases.BaseResponse<int>
+    {
+        public int? FatherId { get; set; }
+        public int RefId { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public int? FatherCode { get; set; }
+        public string FatherName { get; set; }
+    }
+}
